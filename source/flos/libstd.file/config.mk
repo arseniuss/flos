5c5bcf5bdb9785003e87b3534d9e102573ab93c4
Armands Arseniuss Skolmeisters <arseniuss@arseniuss.id.lv>
2019-10-22 21:23:15 +0300

    libstd.file: fixed build process

--- source/flos/libstd.file/config.mk
+++ source/flos/libstd.file/config.mk
@@ -1,25 +1,30 @@
 #
-#	Standard file manipulation library for cell language
-#	Copyright (c) Armands Arseniuss Skolmeisters, All rights reserved.
+#    Standard file manipulation library for cell language
+#    Copyright (C) 2019  Armands Arseniuss Skolmeisters
 #
-#	This library is free software; you can redistribute it and/or
-#	modify it under the terms of the GNU Lesser General Public
-#	License as published by the Free Software Foundation; either
-#	version 3.0 of the License, or (at your option) any later version.
+#    This library is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-#	This library is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#	Lesser General Public License for more details.
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
 #
-#	You should have received a copy of the GNU Lesser General Public
-#	License along with this library.
-#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this library  If not, see <https://www.gnu.org/licenses/>.
 
-CC	    = clang
-AWK	    = awk
-LD	    = ld
+LIBRARIES = std_file
 
-CFLAGS	    = -Iinclude -Os -Wall -Wextra
-LDFLAGS	    = -s
+std_file_DISTRO = flos
 
+#
+# $_BUILDDIR(1 - arch, 2 - os, 3 - host, 4 - category)
+#
+std_file_BUILDDIR = $(1)-$(2)-$(3)/$(4)
+
+#
+# $_INSTALLDIR(1 - arch, 2 - os, 3 - host, 4 - category, 5 - distro)
+#
+std_file_INSTALLDIR = $(ROOT)/$(1)/$(2)/$(4)
